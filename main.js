d73b807488d99fa6c20ad7f1224363bacf5d7d3b
SahasWh <82063178+SahasWh@users.noreply.github.com>
2022-09-18 15:47:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+status =""
+
+
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+
+function start(){
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects" 
+}
+
+function modelLoaded() {
+    console.log(modelLoaded);
+    status = true;
+}
+
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
